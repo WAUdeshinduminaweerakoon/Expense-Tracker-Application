fd400a1626f04f64800739148f5c779355a9d0c2
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-26 20:32:07 +0530

    add Doughnut add Pie charts

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './graph.css';
+import {Doughnut} from 'react-chartjs-2'
+import {Chart,ArcElement}from 'chart.js';
+
+Chart.register(ArcElement);
+const config={
+  data:{
+    datasets: [{
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4,
+      borderRadius:30,
+      spacing:10
+    }]
+
+  },
+  options :{
+    cutout: 115
+  }
+
+}
+
 
 const Graph = () => {
   return (
     <div className='graph-container'>
       <div className="item">
           <div className="chart">
-            Chart
+            <Doughnut{...config}></Doughnut>
+            <h3 className='total-h'>Total</h3>
+            <span>${0}</span>
           </div>
           <div className="lables-div">
             {/*Lables*/}
