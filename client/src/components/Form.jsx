f0f97bd54fc5e88aacb2f85b176bb6c149b8117e
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-09-13 15:43:20 +0530

    form componed all add

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 import './form.css';
 
 const Form = () => {
+
+  const{register, handleSubmit,resetField} = useForm();
+
+  const onSubmit=(data)=>{
+    console.log(data)
+  }
+
+
   return (
     <div className='form-main'>
       <h1 className='form-heard'>Transaction</h1>
-      <form id="form">
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
         <div className='grid gap-4'>
           <div className='input-gruop'>
              <input type="text" placeholder="Sallary,House Read, SIP" className='form-input'/>
           </div>
-          <select className='from-input'>
+          <select className='from-input' {...register('name')}>
             <option value='Investment' defaultValue>Investment</option>
             <option value='Expense' defaultValue>Expense</option>
             <option value='Savings' defaultValue>Savings</option>
           </select>
           <div className='input-gruop'>
-             <input type="text" placeholder="Amount" className='form-input'/>
+             <input type="text" {...register('amount')} placeholder="Amount" className='form-input'/>
           </div>
           <div className="submit-btn">
           <button className="button-submit">Make Transaction</button>
